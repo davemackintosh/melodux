8eca46470bb4a154f6cd74232ff39d26be2d22cc
davemackintosh <me@davemackintosh.co.uk>
2022-03-05 22:54:35 +0000

    Seems to work

--- Sources/Melodux/Melodux.swift
+++ Sources/Melodux/Melodux.swift
@@ -1,6 +1,39 @@
-public struct Melodux {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+final class Store<Actions, StateShape> {
+	var state: StateShape
+	private var rootReducer: (Actions, StateShape) -> StateShape
+	private var middlewares: [(Actions) -> Actions] = []
+
+	init(
+		initialState: StateShape,
+		rootReducer: @escaping (Actions, StateShape) -> StateShape
+	) {
+		self.state = initialState
+		self.rootReducer = rootReducer
+	}
+
+	func registerMiddlewares(middlewares: [(Actions) -> Actions]) -> Store<Actions, StateShape> {
+		self.middlewares = middlewares
+		return self
+	}
+
+	static func composeReducers(reducers: [(Actions, StateShape) -> StateShape]) -> (Actions, StateShape) -> StateShape {
+		return { action, state in
+			return reducers.reduce(state, { prev, next in
+				next(action, prev)
+			})
+		}
+	}
+
+	func dispatch(action: Actions, background: Bool?) {
+		let currentState = self.state
+		let nextAction: Actions = self.middlewares.reduce(action, { prevAction, currentMiddleware in
+			return currentMiddleware(prevAction)
+		})
+		let nextState = self.rootReducer(nextAction, currentState)
+		self.state = nextState
+	}
 }
+
+
--- Tests/MeloduxTests/MeloduxTests.swift
+++ Tests/MeloduxTests/MeloduxTests.swift
@@ -1,11 +1,59 @@
 import XCTest
 @testable import Melodux
 
+struct TestStateShape {
+	var name: String
+	var version: String
+}
+
+enum TestStateActions {
+	case SetName(String)
+	case SetVersion(String)
+}
+
+func rootReducer(action: TestStateActions, state: TestStateShape) -> TestStateShape {
+	switch (action) {
+	case .SetName(let name):
+		return TestStateShape(name: name, version: state.version)
+	case .SetVersion(let version):
+		return TestStateShape(name: state.name, version: version)
+	}
+}
+
 final class MeloduxTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Melodux().text, "Hello, World!")
-    }
+	func testBasicStore() throws {
+		let initialState = TestStateShape(name: "", version: "")
+		let store = Melodux.Store<TestStateActions, TestStateShape>(initialState: initialState, rootReducer: rootReducer)
+
+		XCTAssertEqual(store.state.name, "")
+		XCTAssertEqual(store.state.version, "")
+
+		store.dispatch(action: .SetName("Melodux"), background: false)
+		XCTAssertEqual(store.state.name, "Melodux")
+	}
+
+	func testMiddleware() throws {
+		let initialState = TestStateShape(name: "", version: "")
+		var middlewareWasCalled = 0
+		let middlewares: [(TestStateActions) -> TestStateActions] = [
+			{ action in
+				middlewareWasCalled += 1
+				return action
+			}
+		]
+		let store = Melodux.Store<TestStateActions, TestStateShape>(initialState: initialState, rootReducer: rootReducer)
+			.registerMiddlewares(middlewares: middlewares)
+
+		XCTAssertEqual(middlewareWasCalled, 0)
+		XCTAssertEqual(store.state.name, "")
+		XCTAssertEqual(store.state.version, "")
+
+		store.dispatch(action: .SetName("Melodux"), background: false)
+		XCTAssertEqual(middlewareWasCalled, 1)
+		XCTAssertEqual(store.state.name, "Melodux")
+
+		store.dispatch(action: .SetVersion("1.2.3"), background: false)
+		XCTAssertEqual(middlewareWasCalled, 2)
+		XCTAssertEqual(store.state.version, "1.2.3")
+	}
 }
